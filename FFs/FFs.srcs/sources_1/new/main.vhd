1144b3a5a13400dee1fe52ae228235a304418482
Victor Hugo <victorhpantunes@gmail.com>
2020-12-01 01:03:48 -0300

    adding the 16 FF's to main source

--- FFs/FFs.srcs/sources_1/new/main.vhd
+++ FFs/FFs.srcs/sources_1/new/main.vhd
@@ -10,7 +10,7 @@ entity main is
        ff_assinc_D : in STD_LOGIC;
        ff_assinc_clr : in STD_LOGIC;
        ff_assinc_Q: out STD_LOGIC;
-       led : out std_logic_vector (3 downto 0) 
+       led : out std_logic_vector (15 downto 0) 
          );
 
 end main;
@@ -25,22 +25,22 @@ architecture Behavioral of main is
                clr : in STD_LOGIC);
     end component;
 
-    signal q_out0 : std_logic;
-    signal q_out1 : std_logic;
-    signal q_out2 : std_logic;
-    signal q_out3 : std_logic;
---    signal q_out4 : std_logic;
---    signal q_out5 : std_logic;
---    signal q_out6 : std_logic;
---    signal q_out7 : std_logic;
---    signal q_out8 : std_logic;
---    signal q_out9 : std_logic;
---    signal q_out10 : std_logic;
---    signal q_out11 : std_logic;
---    signal q_out12 : std_logic;
---    signal q_out13 : std_logic;
---    signal q_out14 : std_logic;
---    signal q_out15 : std_logic;
+    signal q_out0 : std_logic := '0';
+    signal q_out1 : std_logic := '0';
+    signal q_out2 : std_logic := '0';
+    signal q_out3 : std_logic := '0';
+    signal q_out4 : std_logic := '0';
+    signal q_out5 : std_logic := '0';
+    signal q_out6 : std_logic := '0';
+    signal q_out7 : std_logic := '0';
+    signal q_out8 : std_logic := '0';
+    signal q_out9 : std_logic := '0';
+    signal q_out10 : std_logic := '0';
+    signal q_out11 : std_logic := '0';
+    signal q_out12 : std_logic := '0';
+    signal q_out13 : std_logic := '0';
+    signal q_out14 : std_logic := '0';
+    signal q_out15 : std_logic := '0';
     
 
 begin
@@ -87,8 +87,124 @@ begin
     
     led(3) <= q_out3;
     
-    ---O ff_assinc_Q é a última saída, no caso o bit que foi deslocado
-    ff_assinc_Q <= q_out3;
+     ff_assinc4 : ff_D_preset_clr_assinc port map(
+        D => q_out3,
+        clk => clk,
+        Q => q_out4,       
+        clr => ff_assinc_clr
+    );
+    
+    led(4) <= q_out4;
+    
+    ff_assinc5 : ff_D_preset_clr_assinc port map(
+        D => q_out4,
+        clk => clk,
+        Q => q_out5,       
+        clr => ff_assinc_clr
+    );
+    
+    led(5) <= q_out5;
+    
+    ff_assinc6 : ff_D_preset_clr_assinc port map(
+        D => q_out5,
+        clk => clk,
+        Q => q_out6,       
+        clr => ff_assinc_clr
+    );
+    
+    led(6) <= q_out6;
+    
+    ff_assinc7 : ff_D_preset_clr_assinc port map(
+        D => q_out6,
+        clk => clk,
+        Q => q_out7,       
+        clr => ff_assinc_clr
+    );
+    
+    led(7) <= q_out7;
+    
+    
+    ff_assinc8 : ff_D_preset_clr_assinc port map(
+        D => q_out7,
+        clk => clk,
+        Q => q_out8,       
+        clr => ff_assinc_clr
+    );
+    
+    led(8) <= q_out8;
+    
+    
+    ff_assinc9 : ff_D_preset_clr_assinc port map(
+        D => q_out8,
+        clk => clk,
+        Q => q_out9,       
+        clr => ff_assinc_clr
+    );
+    
+    led(9) <= q_out9;
+    
+    
+    ff_assinc10 : ff_D_preset_clr_assinc port map(
+        D => q_out9,
+        clk => clk,
+        Q => q_out10,       
+        clr => ff_assinc_clr
+    );
+    
+    led(10) <= q_out10;
+    
+    
+    ff_assinc11 : ff_D_preset_clr_assinc port map(
+        D => q_out10,
+        clk => clk,
+        Q => q_out11,       
+        clr => ff_assinc_clr
+    );
+    
+    led(11) <= q_out11;
+    
+    
+    ff_assinc12 : ff_D_preset_clr_assinc port map(
+        D => q_out11,
+        clk => clk,
+        Q => q_out12,       
+        clr => ff_assinc_clr
+    );
+    
+    led(12) <= q_out12;
+    
+    
+    ff_assinc13 : ff_D_preset_clr_assinc port map(
+        D => q_out12,
+        clk => clk,
+        Q => q_out13,       
+        clr => ff_assinc_clr
+    );
+    
+    led(13) <= q_out13;
+    
+    
+    ff_assinc14 : ff_D_preset_clr_assinc port map(
+        D => q_out13,
+        clk => clk,
+        Q => q_out14,       
+        clr => ff_assinc_clr
+    );
+    
+    led(14) <= q_out14;
+    
+    
+    
+    ff_assinc15 : ff_D_preset_clr_assinc port map(
+        D => q_out14,
+        clk => clk,
+        Q => q_out15,       
+        clr => ff_assinc_clr
+    );
+    
+    led(15) <= q_out15;
+
+    ff_assinc_Q <= q_out15;
     
     
         
